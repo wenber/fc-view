c8eb4bb80ee5c2e85a78278c43acb5c912d544fa
wangkemiao <wangkemiao@baidu.com>
2014-12-06 18:13:50 +0800

    fix deps

--- src/main.js
+++ src/main.js
@@ -6,16 +6,18 @@
 define(function (require) {
     'use strict';
 
+    var _ = require('underscore');
+
     /**
      * fc-view 模块
      * @module view
      */
     var view = {
-        version: '0.0.1-alpha.7'
+        version: '0.0.1-alpha.8'
     };
-    
+
     // 在此直接屏蔽掉er.View的dispose，它会让container内容清空
-    require('er/View').prototype.dispose = function () {};
+    require('er/View').prototype.dispose = _.noop;
 
     return view;
 });
