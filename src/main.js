32a2399f9dda267ffb55ff74dee7a099e1de5f0b
wangkemiao <wangkemiao@baidu.com>
2014-12-24 15:03:15 +0800

    bump 0.0.1-beta

--- src/main.js
+++ src/main.js
@@ -13,7 +13,7 @@ define(function (require) {
      * @module view
      */
     var view = {
-        version: '0.0.1-alpha.17'
+        version: '0.0.1-alpha.18'
     };
 
     // 在此直接屏蔽掉er.View的dispose，它会让container内容清空
